8af6a8e3de5bcfe0afa200ea48cfebdcdd7d210a
Saska Rauhala <saskarauhala@MacBook-Pro-4.home>
2020-04-13 14:25:42 +0300

    make it better 20

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
