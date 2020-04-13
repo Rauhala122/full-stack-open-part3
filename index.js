ef90dfc852f00b0d14fa3189c8f4d02cd5e29705
Saska Rauhala <saskarauhala@MacBook-Pro-4.home>
2020-04-13 10:00:15 +0300

    make it better 13

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const morgan = require("morgan")
-require("dotenv").config()
 
 const app = express()
 const cors = require("cors")
@@ -12,8 +11,6 @@ app.use(express.static('build'))
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'));
-
-const Person = require("./models/person")
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -59,17 +56,22 @@ app.get("/info", (req, res) => {
 })
 
 app.get("/api/persons", (request, response) => {
-  Person.find({}).then(persons => {
-    response.json(persons.map(person => person.toJSON()))
-  })
+  response.json(persons)
 
 })
 
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  Person.findById(id).then(person => {
-    response.json(person.toJSON())
+  const person = persons.find(person => {
+    return person.id === id
   })
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+  console.log(person.name)
+  console.log(id + " gotten")
 })
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -102,14 +104,14 @@ app.post("/api/persons", (request, response) => {
     }
   }
 
-  const person = new Person ({
+  const person = {
     name: body.name,
-    number: body.number
-  })
+    number: body.number,
+    id: generateId()
+  }
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson.toJSON())
-  })
+  persons = persons.concat(person)
+  response.json(person)
 
 })
 
@@ -119,7 +121,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const port = process.env.PORT
+const port = process.env.PORT || 3001
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
