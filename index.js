b7c6dcee61945c91464d98f2e7804820c71b2bbe
Saska Rauhala <saskarauhala@MacBook-Pro-4.home>
2020-04-15 21:28:42 +0300

    fixes on part3

--- index.js
+++ index.js
@@ -1,10 +1,212 @@
-const app = require('./app') // varsinainen Express-sovellus
-const http = require('http')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
+const express = require('express')
+const morgan = require("morgan")
+require('dotenv').config()
+const app = express()
+const cors = require("cors")
 
-const server = http.createServer(app)
 
-server.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
+app.use(express.json())
+app.use(morgan('tiny'))
+app.use(cors())
+app.use(express.static('build'))
+
+
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'));
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator');
+
+mongoose.set('useFindAndModify', false)
+
+const url =
+  `mongodb+srv://rauhala:tarkman51@cluster0-y4sfi.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    required: true
+  }
+})
+
+personSchema.plugin(uniqueValidator);
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+app.use(requestLogger)
+
+let persons = [
+    {
+      "name": "Arto Hellas",
+      "number": "040-123456",
+      "id": 1
+    },
+    {
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523",
+      "id": 2
+    },
+    {
+      "name": "Dan Abramov",
+      "number": "12-43-234345",
+      "id": 3
+    },
+    {
+      "name": "Mary Poppendieck",
+      "number": "39-23-6423122",
+      "id": 4
+    }
+]
+
+app.get("/", (req, res) => {
+  res.send("<h2>Hello world </h2>")
+})
+
+app.get("/info", (req, res) => {
+  res.send(`<p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+    `)
+})
+
+app.get("/api/persons", (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons.map(person => person.toJSON()))
+  })
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  Person.findById(request.params.id).then(person => {
+    if (person) {
+      response.json(person.toJSON())
+    } else {
+      response.status(404).end()
+    }
+  })
+  .catch(error => {
+    console.log("ERROR ", error)
+    response.status(400).send({ error: 'malformatted id' })
+  })
+  .catch(error => next(error))
+})
+
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+const generateId = () => {
+  const getRandomInt = (max) => {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  return getRandomInt(1000000)
+}
+
+app.post("/api/persons", (request, response, next) => {
+  const body = request.body
+
+  // Person.find({}).then(result => {
+  //   console.log("Phonebook")
+  //
+  //   result.forEach(person => {
+  //     if (person.name === body.name) {
+  //       isAlreadyCreated = true
+  //       return response.status(400).json({
+  //            error: 'this persons has already been added'
+  //        })
+  //     }
+  //   })
+  //
+  // })
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  Person.find({ name: body.name }).then(persons => {
+    if (persons.length === 0 ) {
+
+      person.save()
+        .then(savedPerson =>  savedPerson.toJSON())
+        .then(savedAndFormattedPerson => {
+          response.json(savedAndFormattedPerson)
+      })
+        .catch(error => next(error))
+
+    } else {
+      return response.status(400).json({
+        error: 'this persons has already been added'
+      })
+    }
+  })
+
+})
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson.toJSON())
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind == 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const port = process.env.PORT | 3001
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
 })
